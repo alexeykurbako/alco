85d28664420112efa412166019c98b30315933dc
alexeykurbako <36738501+alexeykurbako@users.noreply.github.com>
2018-03-01 21:25:59 +0300

    Code Review 2

--- alcoJS.js
+++ alcoJS.js
@@ -1,51 +1,79 @@
-
-function operation(oper,ch2){
+function getOperator(oper, ch2) {
 	switch (oper) {
 		case 0:
-		//if(ch2<0) return "";
-			return "+";break;
+      if (ch2 < 0) {
+        return "";
+      }
+      return "+";
+      break;
 		case 1:
-		//if(ch2<0) return "+";
-			return "-";break;
-		case 2: return "*"; break;
-		case 3: return "/"; break;
+	    if (ch2 < 0) {
+        return "+";
+      }
+      return "-";
+      break;
+		case 2:
+      return "*";
+      break;
+		case 3:
+      return "/";
+      break;
 	}
 }
-function prov(ch2,oper){
-	if(oper==1 && ch2<0){
-		const sk=1;
-		return -ch2;
-		}
-		return ch2;
+function checkNumber(ch2, oper) {
+	if (oper == 0 && ch2 < 0) {
+    return ch2;
+  }
+	if (oper == 1 && ch2 < 0) {
+    return -ch2;
+  }
+	if ((oper == 2 || oper == 3) && ch2 < 0) {
+    return `(${ch2})`;
+  }
+	return ch2;
 }
-function result(ch1,ch2,oper){
+function randomOperator() {
+	return Math.floor(Math.random()*4);
+}
+
+function getResult(ch1, ch2, oper) {
 	switch (oper) {
-		case 0: return rez = Math.floor(ch1+ch2); break;
-		case 1: return rez = Math.floor(ch1-ch2); break;
-		case 2: return rez = ch1*ch2; break;
-		case 3: return rez = Math.floor((ch1/ch2)*10)/10; break;
+		case 0: 
+			return rez = Math.floor(ch1 + ch2); 
+			break;
+		case 1:
+			return rez = Math.floor(ch1 - ch2); 
+			break;
+		case 2: 
+			return rez = ch1 * ch2;
+			break;
+		case 3:
+      return rez = Math.floor((ch1 / ch2) * 10) / 10; break;
 	}
 }
-function getrandom(min,max){
-	const result = Math.floor(Math.random()*(max-min))+min;
-	if(result==0)return max;
-	return result;
+
+function getrandom() {
+	const min = -10;
+	const max = 10;
+	const result = Math.floor(Math.random() * (max - min)) + min;
+	if (result == 0) {
+    return max;
+  }
+  return result;
 }
-while(true){
-const min=-10;
-const max= 10;
-const ch1 = getrandom(min,max);
-const ch2 = getrandom(min,max);
-const oper = Math.floor(Math.random()*4);
-	const temp = prompt(`${ch1}${operation(oper,ch1,ch2)}${ch2}=?`);
-	if(temp==result(ch1,ch2,oper)){
+
+while (true) {
+  const ch1 = getrandom();
+  const ch2 = getrandom();
+  const oper = randomOperator();
+  const result = getResult(ch1, ch2, oper);
+	const answer = prompt(`${ch1} ${getOperator(oper, ch2)} ${checkNumber(ch2, oper)} = ?`);
+	if (+answer == result) {
 		alert('ещё по одной...');
-	}
-	else
-		{
+	} else {
 		alert('Хватит с тебя');
 		break;
-		}
+	}
 }
 
 	
